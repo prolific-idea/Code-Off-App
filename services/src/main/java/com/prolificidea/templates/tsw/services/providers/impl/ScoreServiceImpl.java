bd6e3df40f5bee1ab1ead6d35cf65054def01d23
matthewJ1299 <matthew.j@live.com>
2016-03-02 20:38:26 +0200

    added first iteration of scoring

--- services/src/main/java/com/prolificidea/templates/tsw/services/providers/impl/ScoreServiceImpl.java
+++ services/src/main/java/com/prolificidea/templates/tsw/services/providers/impl/ScoreServiceImpl.java
@@ -1,11 +1,15 @@
 package com.prolificidea.templates.tsw.services.providers.impl;
 
 import com.prolificidea.templates.tsw.domain.entities.Entry;
+import com.prolificidea.templates.tsw.services.DTOs.EntryDTO;
+import com.prolificidea.templates.tsw.services.DTOs.PersonDTO;
 import com.prolificidea.templates.tsw.services.providers.EntryService;
 import com.prolificidea.templates.tsw.services.providers.PersonService;
 import com.prolificidea.templates.tsw.services.providers.ScoreService;
 import com.prolificidea.templates.tsw.services.providers.TechnologyService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * Created by matthew.jordaan on 2016/03/01.
@@ -22,22 +26,21 @@ public class ScoreServiceImpl implements ScoreService {
     TechnologyService technologyService;
 
 
-    //
-    //Max 2 points per challenge
-    //
-    //
-    //
-    //
-    //
-    //
-    //
-    //
+    public void addScore(EntryDTO entry) {
+        List<EntryDTO> entries = entryService.findAllEntriesByPersonAndChallenge(entry.getPersonId(),entry.getChallengeId());
+        PersonDTO p = personService.findPerson(entry.getPersonId());
+        int score = 0;
+        for (EntryDTO e : entries)
+        {
+             score += e.getResult();
+        }
 
-//
-    public void addScore(Entry entry) {
-//        Person person = personService.findPerson(entry.getPersonId());
-//
-//
+        if (score > 2) score = 2;
+        p.setScore(p.getScore() + score);
+        personService.updatePerson(p);
 
+//        if (entries.size() == 1) {
+//            p.setScore(p.getScore() + entry.getResult());
+//        }
     }
 }
