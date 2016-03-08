ce4d9f4445518ae77db529d907e4e99b3ececec7
Teshikajin <theobad007@gmail.com>
2016-03-08 12:52:23 +0200

    Added sql query for technologies

---
+++ sql/InsertTechs.sql
@@ -0,0 +1,31 @@
+/*
+-- Query: SELECT * FROM dbo.technology
+LIMIT 0, 1000
+
+-- Date: 2016-03-08 12:43
+*/
+UPDATE entry SET techId = 15 where techId > 23
+
+INSERT INTO `technology` (`techId`,`description`) VALUES (1,'JavaScript');
+INSERT INTO `technology` (`techId`,`description`) VALUES (2,'Java');
+INSERT INTO `technology` (`techId`,`description`) VALUES (3,'Ruby');
+INSERT INTO `technology` (`techId`,`description`) VALUES (4,'PHP');
+INSERT INTO `technology` (`techId`,`description`) VALUES (5,'Python');
+INSERT INTO `technology` (`techId`,`description`) VALUES (6,'VB .NET');
+INSERT INTO `technology` (`techId`,`description`) VALUES (7,'C++');
+INSERT INTO `technology` (`techId`,`description`) VALUES (8,'C#');
+INSERT INTO `technology` (`techId`,`description`) VALUES (9,'C');
+INSERT INTO `technology` (`techId`,`description`) VALUES (10,'TCL');
+INSERT INTO `technology` (`techId`,`description`) VALUES (11,'Objective-C');
+INSERT INTO `technology` (`techId`,`description`) VALUES (12,'Shell');
+INSERT INTO `technology` (`techId`,`description`) VALUES (13,'Perl');
+INSERT INTO `technology` (`techId`,`description`) VALUES (14,'VimL');
+INSERT INTO `technology` (`techId`,`description`) VALUES (15,'Emacs Lisp');
+INSERT INTO `technology` (`techId`,`description`) VALUES (16,'Erlang');
+INSERT INTO `technology` (`techId`,`description`) VALUES (17,'Haskell');
+INSERT INTO `technology` (`techId`,`description`) VALUES (18,'Scala');
+INSERT INTO `technology` (`techId`,`description`) VALUES (19,'Go');
+INSERT INTO `technology` (`techId`,`description`) VALUES (20,'Clojure');
+INSERT INTO `technology` (`techId`,`description`) VALUES (21,'Lua');
+INSERT INTO `technology` (`techId`,`description`) VALUES (22,'Bash');
+INSERT INTO `technology` (`techId`,`description`) VALUES (23,'R');
