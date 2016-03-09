c7ce318da86df03e2f6da7355f9fdbacf18a78ba
Theodore Badenhorst <theodore.badenhorst@entelect.co.za>
2016-03-09 10:26:13 +0200

    Added paging to the leader board and fixed the paging of challenges.

---
+++ services/src/main/java/com/prolificidea/templates/tsw/services/DTOs/PersonCountDTO.java
@@ -0,0 +1,13 @@
+package com.prolificidea.templates.tsw.services.DTOs;
+
+public class PersonCountDTO {
+    long countOfPerson;
+
+    public long getCountOfPerson() {
+        return countOfPerson;
+    }
+
+    public void setCountOfPerson(long countOfPerson) {
+        this.countOfPerson = countOfPerson;
+    }
+}
