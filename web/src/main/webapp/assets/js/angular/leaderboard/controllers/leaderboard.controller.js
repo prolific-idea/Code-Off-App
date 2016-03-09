781b9f0ec403ecd59f897aae5fa65b512d165b85
szymon.welgus <szymon.welgus@entelect.co.za>
2016-03-09 11:40:07 +0200

    Added angular things for searching and filtering by

--- web/src/main/webapp/assets/js/angular/leaderboard/controllers/leaderboard.controller.js
+++ web/src/main/webapp/assets/js/angular/leaderboard/controllers/leaderboard.controller.js
@@ -60,81 +60,93 @@
 //}());
 
 (function () {
-    var app = angular.module("codeOffLeaderboard");
+	var app = angular.module("codeOffLeaderboard");
 
-    app.controller("viewLeaderboardController",
-        function ($scope, $http, $log, Coders, PersonCount) {
-            var ctrl = $scope;
-            var pageSize = 6;
-            ctrl.pageNum = 1;
-            ctrl.codersTemp = Coders.getPagedLeaderboard({pageNum: ctrl.pageNum, pageSize: pageSize});
-            ctrl.personCountTemp = PersonCount.count();
-            ctrl.personCountTemp.$promise.then(function () {
-                ctrl.countOfCoders = ctrl.personCountTemp;
-                ctrl.countOfCoders = ctrl.countOfCoders.countOfPerson;
-            }, $log.error);
-            ctrl.codersTemp.$promise.then(function () {
-                ctrl.coders = ctrl.codersTemp;
-                ctrl.ChangeTechDescription();
-                console.log(ctrl.coders);
-                var lastPage = Math.ceil(ctrl.countOfCoders / pageSize);
-                if (lastPage === ctrl.pageNum)
-                    ctrl.CanNotNext = true;
-            }, $log.error);
+	app.controller("viewLeaderboardController",
+		function ($scope, $http, $log, Coders, PersonCount) {
+			var ctrl = $scope;
+			var pageSize = 6;
+			ctrl.pageNum = 1;
+
+			ctrl.searchTerm = "";
+			ctrl.searchActions = [
+				{id: 'filter', name: 'Filter'},
+				{id: 'all', name: 'All'},
+				{id: 'challenge', name: 'Challenge'},
+				{id: 'technology', name: 'Technology'}
+			];
+			ctrl.selectedSearch = ctrl.searchActions[0];
+
+			ctrl.setSearchAction = function(action) {
+				ctrl.selectedSearch = action;
+			}
+
+			ctrl.searchFor = function() {
+				console.log(ctrl.selectedSearch.id);
+			}
+
+			ctrl.codersTemp = Coders.getPagedLeaderboard({pageNum: ctrl.pageNum, pageSize: pageSize});
+			ctrl.personCountTemp = PersonCount.count();
+			ctrl.personCountTemp.$promise.then(function () {
+				ctrl.countOfCoders = ctrl.personCountTemp;
+				ctrl.countOfCoders = ctrl.countOfCoders.countOfPerson;
+			}, $log.error);
+			ctrl.codersTemp.$promise.then(function () {
+				ctrl.coders = ctrl.codersTemp;
+				ctrl.ChangeTechDescription();
+				console.log(ctrl.coders);
+				var lastPage = Math.ceil(ctrl.countOfCoders / pageSize);
+				if (lastPage === ctrl.pageNum)
+					ctrl.CanNotNext = true;
+			}, $log.error);
 
 
-            ctrl.ToNextPage = function () {
-                ctrl.pageNum += 1;
-                ctrl.codersTemp = Coders.getPagedLeaderboard({pageNum: ctrl.pageNum, pageSize: pageSize});
-                ctrl.codersTemp.$promise.then(function () {
-                    ctrl.coders = ctrl.codersTemp;
-                    ctrl.ChangeTechDescription();
-                    var lastPage = Math.ceil(ctrl.countOfCoders / pageSize);
-                    if (lastPage === ctrl.pageNum)
-                        ctrl.CanNotNext = true;
-                    else
-                        ctrl.CanNotNext = false;
-                }, $log.error);
-            };
+			ctrl.ToNextPage = function () {
+				ctrl.pageNum += 1;
+				ctrl.codersTemp = Coders.getPagedLeaderboard({pageNum: ctrl.pageNum, pageSize: pageSize});
+				ctrl.codersTemp.$promise.then(function () {
+					ctrl.coders = ctrl.codersTemp;
+					ctrl.ChangeTechDescription();
+					var lastPage = Math.ceil(ctrl.countOfCoders / pageSize);
+					ctrl.CanNotNext = lastPage === ctrl.pageNum;
+				}, $log.error);
+			};
 
-            ctrl.ToPrevPage = function () {
-                if (ctrl.pageNum != 1) {
-                    ctrl.pageNum -= 1;
-                    ctrl.codersTemp = Coders.getPagedLeaderboard({pageNum: ctrl.pageNum, pageSize: pageSize});
-                    ctrl.codersTemp.$promise.then(function () {
-                        ctrl.coders = ctrl.codersTemp;
-                        ctrl.ChangeTechDescription();
-                        var lastPage = Math.ceil(ctrl.countOfCoders / pageSize);
-                        if (lastPage === ctrl.pageNum)
-                            ctrl.CanNotNext = true;
-                        else
-                            ctrl.CanNotNext = false;
-                    }, $log.error);
-                }
-            };
+			ctrl.ToPrevPage = function () {
+				if (ctrl.pageNum != 1) {
+					ctrl.pageNum -= 1;
+					ctrl.codersTemp = Coders.getPagedLeaderboard({pageNum: ctrl.pageNum, pageSize: pageSize});
+					ctrl.codersTemp.$promise.then(function () {
+						ctrl.coders = ctrl.codersTemp;
+						ctrl.ChangeTechDescription();
+						var lastPage = Math.ceil(ctrl.countOfCoders / pageSize);
+						ctrl.CanNotNext = lastPage === ctrl.pageNum;
+					}, $log.error);
+				}
+			};
 
-            ctrl.ChangeTechDescription = function () {
-                for (var i = 0; i < ctrl.coders.length; i++) {
-                    console.log(ctrl.coders[i].techs);
-                    for (var j = 0; j < ctrl.coders[i].techs.length; j++) {
-                        console.log(ctrl.coders[i].techs[j]);
+			ctrl.ChangeTechDescription = function () {
+				for (var i = 0; i < ctrl.coders.length; i++) {
+					console.log(ctrl.coders[i].techs);
+					for (var j = 0; j < ctrl.coders[i].techs.length; j++) {
+						console.log(ctrl.coders[i].techs[j]);
 
-                        if (ctrl.coders[i].techs[j].description === "C#") {
-                            console.log(true);
-                            ctrl.coders[i].techs[j].description = "Csharp";
-                        }
-                        if (ctrl.coders[i].techs[j].description === "C++") {
-                            console.log(true);
-                            ctrl.coders[i].techs[j].description = "Cplusplus";
-                        }
-                        if (ctrl.coders[i].techs[j].description === "Objective-C") {
-                            console.log(true);
-                            ctrl.coders[i].techs[j].description = "objc";
-                        }
+						if (ctrl.coders[i].techs[j].description === "C#") {
+							console.log(true);
+							ctrl.coders[i].techs[j].description = "Csharp";
+						}
+						if (ctrl.coders[i].techs[j].description === "C++") {
+							console.log(true);
+							ctrl.coders[i].techs[j].description = "Cplusplus";
+						}
+						if (ctrl.coders[i].techs[j].description === "Objective-C") {
+							console.log(true);
+							ctrl.coders[i].techs[j].description = "objc";
+						}
 
-                        ctrl.coders[i].techs[j].description = ctrl.coders[i].techs[j].description.toLowerCase();
-                    }
-                }
-            }
-        });
+						ctrl.coders[i].techs[j].description = ctrl.coders[i].techs[j].description.toLowerCase();
+					}
+				}
+			}
+		});
 })();
